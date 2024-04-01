2b6e4c0210b0ae34b7659ed07a16ce9e458fbcf6
Smimid Sofian <sofian.smimid@etu.unilim.fr>
2024-04-02 00:10:51 +0200

    initial commit with first page and gifs for the buttons

---
+++ SofianApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SofianApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ SofianApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace SofianApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+            var images = new List<string>
+            {
+                "security1.png",
+                "security2.png",
+                "security3.png"
+            };
+            
+            async void OnAcceptButtonClicked(object sender, EventArgs e)
+            {
+                await Navigation.PushAsync(new AcceptPage());
+            }
+            
+            async void OnDeclineButtonClicked(object sender, EventArgs e)
+            {
+                await Navigation.PushAsync(new DeclinePage());
+            }
+
+        }
+    }
+}
